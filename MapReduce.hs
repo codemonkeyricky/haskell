c317f41f34195a87efa2386161c10175aeaf7a3f
Richard Tang <code.monkey.ricky@gmail.com>
2025-04-06 16:49:47 -0700

    snapshot

--- MapReduce.hs
+++ MapReduce.hs
@@ -55,19 +55,19 @@ node :: PortNumber -> String -> IO ()
 node port peer = do
   let eventLoop port chan =
         forever $ do
-          (sock, msg) <- readChan chan
+          (tx, msg) <- readChan chan
           case msg of
-            NewConnection id -> print "test"
+            NewConnection id      -> print "test"
             GossipRequest cluster -> print "test"
-            Ping -> do
-              sendAll sock (DBL.toStrict $ serialize Pong)
+            -- Ping -> do
+            --   sendAll sock (DBL.toStrict $ serialize Pong)
             -- AddNode -> do
             --   void $ forkIO $ node (port + 1)
             --   eventLoop (port + 1) chan
   let connAcceptor sock chan =
         forever $ do
           conn <- accept sock
-          forkIO (rxHandler conn chan)
+          forkIO (rxPacket conn chan)
   -- create socket and channel
   chan <- newChan
   sock <- socket AF_INET Stream defaultProtocol
@@ -81,16 +81,23 @@ node port peer = do
   _ <- forkIO $ eventLoop port chan
   -- connection forker
   _ <- forkIO $ connAcceptor sock chan
-  when (peer /= "") $ do
-    let (host, portStr) = break (== ':') peer
-    let peerPort = read (drop 1 portStr) :: PortNumber
-    sockToPeer <- socket AF_INET Stream defaultProtocol
-    connect sockToPeer (SockAddrInet peerPort 0)
-    writeChan chan (sockToPeer, Ping)
+  -- when (peer /= "") $ do
+  --   let (host, portStr) = break (== ':') peer
+  --   let peerPort = read (drop 1 portStr) :: PortNumber
+  --   sockToPeer <- socket AF_INET Stream defaultProtocol
+  --   connect sockToPeer (SockAddrInet peerPort 0)
+  --   writeChan chan (sockToPeer, Ping)
   print "node create complete"
 
-rxHandler :: (Socket, SockAddr) -> Chan (Socket, Message) -> IO ()
-rxHandler (sock, _) chan = do
+rxEvent :: Socket -> Chan (Chan Message, Message) -> Message -> IO ()
+rxEvent sock tx msg = do
+  rx <- newChan
+  writeChan tx (rx, msg)
+  to_send <- readChan rx
+  sendAll sock (DBL.toStrict $ serialize to_send)
+
+rxPacket :: (Socket, SockAddr) -> Chan (Chan Message, Message) -> IO ()
+rxPacket (sock, _) tx = do
   handle (\(SomeException _) -> return ())
     $ fix
     $ \loop -> do
@@ -99,8 +106,8 @@ rxHandler (sock, _) chan = do
           then return ()
           else do
             case deserialize (DBL.fromStrict msg) of
-              Just event -> writeChan chan (sock, event)
-              Nothing    -> print "Invalid message!"
+              Just evt -> void $ forkIO (rxEvent sock tx evt)
+              Nothing  -> print "Invalid message!"
             loop
 
 main :: IO ()
